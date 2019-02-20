aa4e4bac4e37c47b354aa7e9c56204df6b4c9b32
Andrew Stacey <koalasthegreat@gmail.com>
2019-02-20 13:40:39 -0330

    Temporary solution to bot leaving voice and added .gitignore for token

--- bot.py
+++ bot.py
@@ -1,39 +1,44 @@
 import discord
 import os
+import sys
 from discord.ext import commands
 
 TOKEN = open("TOKEN").readline()
 
-client = commands.Bot(command_prefix='gnome, ')
+bot = commands.Bot(command_prefix=['Gnome, ', 'gnome, '], description="A Soundboard Bot")
 
-@client.async_event
-def on_ready():
+@bot.event
+async def on_ready():
     print("Gnome Boi is ready to meme.")
 
-@client.async_event
-def on_message(message):
-    author = message.author
-    content = message.content
-    channel = message.channel
-    vc = author.voice_channel
+@bot.command()
+async def laugh(ctx):
+    voice = ctx.author.voice
+    await ctx.send("**HOOOH**")
 
-    if client.command_prefix + "laugh" in content.lower():
-        yield from client.send_message(channel, "**HOOOH**")
+    if voice is None:
+        await ctx.send("Hello there, old chum. Not caring to chat with an old friend?")
+    else:
+        playing = True
 
-        if vc is None:
-            yield from client.send_message(channel, "Hello there, old chum. Not caring to chat with an old friend?")
-        else:
-            voice = yield from client.join_voice_channel(vc)
-            player = voice.create_ffmpeg_player('hooh.mp3')
-            player.start()
+        clip = discord.FFmpegPCMAudio('hooh.mp3')
+        client = await voice.channel.connect()
+        client.play(clip)
 
-            playing = True
-            
-            while(playing):
-                if player.is_done():
-                    playing = False
-            player.stop()
+        while playing:
+            if not client.is_playing():
+                await client.disconnect()
+                playing = False
 
-            yield from voice.disconnect()
+@bot.command()
+async def leave(ctx):
+    person = ctx.author
 
-client.run(TOKEN)
+    if person.id == 153935534207533056:
+        await ctx.send("Goodbye then, old chum!")
+        sys.exit()
+    else:
+        await ctx.send("No way!")
+
+
+bot.run(TOKEN)
